b0131da36984ce1d3c83f661b528aec81ed4850d
Nie Saleh <145161540+nie78@users.noreply.github.com>
2025-10-07 00:15:56 +0300

    main program

--- functions.py
+++ functions.py
@@ -32,3 +32,38 @@ def fibonacci(m):#o(2^n)
         return 1;
     else:
         return fibonacci(m-1)+fibonacci(m-2);
+
+
+
+while(True):
+  choice = input("Choose an option:")
+  print("1. Factorial")
+  print("2. Find Max")
+  print("3. Linear Search")
+  print("4. Fibonacci")
+  print("5. Login")
+  print("6. Exit")
+  if(choice=="1"):
+      n=factorial(4)
+      print(" the factorial of 4 is  ",n)
+      break;
+  elif(choice=="2"):
+      n=find_max([1,8,20,7])
+      print("The max of the list [1,8,20,7] is ",n)
+      break;
+  elif(choice=="3"):
+      n=linear_search([2,4,6,9],3)
+      print("Does the list [2,4,6,9] contain 3? ",n)
+      break;
+  elif(choice=="4"):
+      n=fibonacci(5)
+      print("The fibonacci of 5 is ",n)
+      break;
+  elif(choice=="5"):
+      name=input("Enter your name")
+      password=input("Enter your password")
+      print(name, "you are logged in")
+      break;
+  elif(choice=="6"):
+      print("your are exiting ")
+      break;
